7b648f210d7895240650ff9cc3842f14adeb57dd
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2026-02-15 13:37:22 -0500

    Add event listeners for form and submiting data to create new Books

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 //MODEL
 
 // Store book objects in an array
-const myLibrary = [
-  { title: 'Horse', author: 'Jimmy', pages: 22, isRead: false, id: 1 },
-];
+const myLibrary = [];
 
 // Build the Constructor for Book
-function Book(title, author, pages, isRead) {
+function Book(title, author, currentPage, totalPages, isRead) {
   // add safeguard
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
@@ -14,13 +12,14 @@ function Book(title, author, pages, isRead) {
 
   this.title = title;
   this.author = author;
-  this.pages = pages;
+  this.currentPage = currentPage;
+  this.totalPages = totalPages;
   this.isRead = isRead;
   this.id = crypto.randomUUID();
 }
 
 Book.prototype.sayInfo = function () {
-  return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  return `The ${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`;
 };
 
 function addBookToLibrary(bookInstance) {
@@ -39,11 +38,12 @@ function createBookCard(book) {
     <div class="leftSide">
       <h1>${book.title}</h1>
       <p>${book.author}</p>
-      <p>${book.pages}</p>
+      <p>${book.currentPage}</p>
+      <p>${book.totalPages}</p>
     </div>
     <div class="rightSide">
       <button class="isReadOrNot">Status</button>
-      <button class="deleteButton">Delete</button>
+      <button class="deleteButton" data-book-id="${book.id}">Delete</button>
       <button class="expand">Expand</button>
     </div>
   `;
@@ -51,38 +51,51 @@ function createBookCard(book) {
   return card;
 }
 
-// CONTROLLER
-function init() {
-  const collectContainer = document.querySelector('.container');
-  const addButton = document.querySelector('.addButton');
+function renderLibrary() {
+  const collectionContainer = document.querySelector('#BookContainer');
+  collectionContainer.innerHTML = ``;
 
-  console.log(addButton);
-
-  addButton.addEventListener('click', () => {
-    const newBook = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-
-    addBookToLibrary(newBook);
-  });
-  container.appendChild();
-}
-
-function checkLibrary() {
   myLibrary.forEach((item) => {
-    const collectionContainer = document.querySelector('#BookContainer');
-    collectionContainer.innerHTML = ``;
-
+    console.log(item);
     collectionContainer.appendChild(createBookCard(item));
   });
 }
 
+function start() {
+  renderLibrary();
 
-function start(){
-  checkLibrary();
   const addButton = document.querySelector('.addButton');
+  const dialog = document.querySelector('#bookDialog');
+  const bookForm = document.querySelector('#bookForm');
 
-  addButton.addEventListener('click', ()=>{
-    
-  })
+  addButton.addEventListener('click', () => {
+    dialog.showModal();
+  });
+
+  bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const bookName = document.querySelector('#book_name').value;
+    const bookAuthor = document.querySelector('#book_author').value;
+    const currentPage = document.querySelector('#current_page').value;
+    const totalPages = document.querySelector('#total_pages').value;
+    const isRead = document.querySelector('#currently_reading_state').checked;
+
+    let newBook = new Book(
+      bookName,
+      bookAuthor,
+      currentPage,
+      totalPages,
+      isRead,
+    );
+
+    addBookToLibrary(newBook);
+    console.log('ADDED NEW BOOK');
+    renderLibrary();
+
+    bookForm.reset();
+    dialog.close();
+  });
 }
 
-// When it comes to adding to the library and displaying it.
+start();
